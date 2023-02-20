3c5512be1e71da8b44b7dc41ddc47d6d6c8e1ad9
yemrecan <yildirimemrecan@outlook.com>
2023-02-20 21:11:13 +0300

    rastgele secenekler fonksiyonu eklendi

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("merhaba");
+// "rock" | "paper" | "scissors" stringlerinden birini rastgele döner
+function getComputerChoice() {
+    // birinci yaklasim
+    // const rastgeleSayi = Math.floor(Math.random() * 3)
+    // if(rastgeleSayi === 0){
+    //     return "rock"
+    // }
+    // else if(rastgeleSayi === 1){
+    //     return "paper"
+    // }
+    // else{
+    //     return "scissors"
+    // }
+
+    //ikinci dizi yaklasimi
+    const secenekler = ["rock", "paper", "scissors"]
+    const rastgeleSayi = Math.floor(Math.random() * 3)  // 0,1,2
+    return secenekler[rastgeleSayi]
+}
+
